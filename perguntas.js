95eba514395fa93a5ca05a0667ff2746be5a7565
camilla1608 <camilla.britto@escola.pr.gov.br>
2025-06-11 14:39:41 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'e do filosofo Descartes'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil e brasilia'
+)
+
+criaCartao(
+    'curiosidade',
+    'De onde é a invenção do chuveiro elétrico?',
+    'vem do Brasil'
+)
+
+criaCartao(
+   
+ 'geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia '
+)
+
+criaCartao(
+   
+ 'curiosidade',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote'
+)
+
+ criaCartao(
+   
+ 'quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    ' 118'
+)
+  
+
+
+
+
